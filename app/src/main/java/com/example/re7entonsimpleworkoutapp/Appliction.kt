fe48480510864fb11423f04bb412ec4ef67efb95
Kianvash <Kianvash.ghahari@gmail.com>
2025-05-11 23:23:51 +0330

    Added application class for hilt registration

---
+++ app/src/main/java/com/example/re7entonsimpleworkoutapp/Appliction.kt
@@ -0,0 +1,8 @@
+package com.example.re7entonsimpleworkoutapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+// Annotate Application to kick off Hilt code generation.
+@HiltAndroidApp
+class Application : Application()
